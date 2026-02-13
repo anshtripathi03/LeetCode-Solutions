ce485a49d7a3d0990ff7db3ad52885c88f94a395
Ansh Tripathi <ansh.tripathi17012006@gmail.com>
2026-02-13 21:45:50 +0530

    Add submission 1918168212

---
+++ submissions/Search in Rotated Sorted Array/1918168212.java
@@ -0,0 +1,35 @@
+# Title: Search in Rotated Sorted Array
+# Submission ID: 1918168212
+# Status: Accepted
+# Date: February 13, 2026 at 09:45:41 PM GMT+5:30
+
+class Solution {
+    public int search(int[] nums, int target) {
+        int left=0;
+        int right=nums.length-1;
+        int mid=0;
+        while(left<=right){
+            mid=(left+right)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            if(nums[left]<=nums[mid]){
+                if(nums[left]<= target && nums[mid]> target){
+                    right=mid-1;
+                }
+                else{
+                    left=mid+1;
+                }
+            }
+            else{
+                if(nums[mid]<target && nums[right]>=target){
+                    left=mid+1;
+                }
+                else{
+                    right=mid-1;
+                }
+            }
+        }
+        return -1;
+    }
+}
